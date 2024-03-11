cc6f662e6b26708b9fa2a97ace9768d1f17d0431
86 Octopus <octopus86@163.com>
2024-03-12 01:08:13 +0800

    Site updated: 2024-03-12 01:08:03

--- js/fomal.js
+++ js/fomal.js
@@ -3174,7 +3174,7 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/dm14.webp);
+    --default-bg: url(https://pic.imgdb.cn/item/65b68694871b83018a91c4f2.jpg);
     --darkmode-bg:url(https://lskypro.acozycotage.net/Fomalhaut/img/yuanshen1.webp);
     --mobileday-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/snow.webp);
     --mobilenight-bg: url(https://lskypro.acozycotage.net/Fomalhaut/img/mb8.webp);
